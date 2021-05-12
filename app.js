17e350e71080609745871551785e336a0a7431b6
Jack <jack_dickinson@icloud.com>
2021-05-12 08:42:00 +0100

    Updated app.js to include the 0 switch case for the 'sunday' to be included when setting the dates of the cards. Also changed the search button border-radius

--- app.js
+++ app.js
@@ -17,10 +17,15 @@ var arrayOfTitles = [card_firstDay_title,card_secondDay_title,card_thirdDay_titl
 // get input from user on submit of button press
 
 submit_search_btn.addEventListener('click', () => {
+    
 var user_input = document.getElementById("location_input").value;
 
 document.getElementById("location_input").value = "";
+
+// carry out data analysis and retrieval 
+
 fetchData(user_input);
+
 });
 
  function fetchData(location) {
@@ -29,55 +34,19 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + location + '&appid
     .then((response) => {
         
         if (response.status == 400) {
-alert("Location not found - Please try inputting a valid city.");
+alert("Location must not be empty - Please try entering a valid city or country");
+        } else if (response.status == 404) {
+            alert("Location not found - Please try inputting a valid city or country.");
         } else {
 
-        
         response.json().then((data) => {
 
             // --data required--
             // min temp
             // max temp
 
-        var firstDate = new Date(String(data.list[0].dt_txt).split(" ")[0]);
-
-            for (var i = 0;i < 5; i++) {
-              var date = new Date();
-              var dayOfWeek = ""
-              date.setDate(firstDate.getDate() + i);
-
-                switch (date.getDay()) {
-                    case 1:
-                        dayOfWeek = "Monday"
-                        break;
-                    case 2:
-                        dayOfWeek = "Tuesday"
-                        break;
-                    case 3:
-                        dayOfWeek = "Wednesday"
-                        break;
-                    case 4:
-                        dayOfWeek = "Thursday"
-                        break;
-                    case 5:
-                        dayOfWeek = "Friday"
-                        break;
-                    case 6:
-                        dayOfWeek = "Saturday"
-                        break;
-                    case 7:
-                        dayOfWeek = "Sunday"
-                        break;
-    
-                    default:
-                        break;
-                }
-
-                console.log(dayOfWeek)
-                // set day of card title
-                arrayOfTitles[i].innerHTML = dayOfWeek
-
-            }
+            // set the days of the week for the card headings 
+           setDaysOfWeek(data);
 
             // get the highest and minimum temperature for any given day (5 day max)
             var temps = [];
@@ -85,13 +54,13 @@ alert("Location not found - Please try inputting a valid city.");
             var temps_lows = [];
 
             for (var i = 0;i < 40;i++) {
-                console.log(String(data.list[i].dt_txt).split(" ")[1])
+              //  console.log(String(data.list[i].dt_txt).split(" ")[1])
                 if (String(data.list[i].dt_txt).split(" ")[1] === "00:00:00") {
                   //  console.log( typeof data.list[i].main.temp);
                     temps_highs.push((Math.max(...temps) - 273.15).toFixed(1))
                     temps_lows.push((Math.min(...temps) - 273.15).toFixed(1))
-                    console.log(temps_highs)
-                    console.log(temps_lows)
+             //       console.log(temps_highs)
+             //       console.log(temps_lows)
                     temps = [];
                     
                 } else {
@@ -107,6 +76,47 @@ alert("Location not found - Please try inputting a valid city.");
 
         })}
     })
+}
+
+function setDaysOfWeek(data) {
+    var firstDate = new Date(String(data.list[0].dt_txt).split(" ")[0]);
+
+    for (var i = 0;i < 5; i++) {
+      var date = new Date();
+      var dayOfWeek = ""
+      date.setDate(firstDate.getDate() + i);
+        switch (date.getDay()) {
+            // 0 is the start of the week according to the api this is a sunday
+            case 0:
+                dayOfWeek = "Sunday"
+                break;
+            case 1:
+                dayOfWeek = "Monday"
+                break;
+            case 2:
+                dayOfWeek = "Tuesday"
+                break;
+            case 3:
+                dayOfWeek = "Wednesday"
+                break;
+            case 4:
+                dayOfWeek = "Thursday"
+                break;
+            case 5:
+                dayOfWeek = "Friday"
+                break;
+            case 6:
+                dayOfWeek = "Saturday"
+                break;
+
+            default:
+                break;
+        }
+
+        // set day of card title
+        arrayOfTitles[i].innerHTML = dayOfWeek
+
+    }
 }
  
 document.getElementById("location_input").value = "London";
